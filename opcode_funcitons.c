ff5f2986583a5c9511028e86d3c2aea8ade563ed
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-20 21:12:26 +0000

    01 | code try | functions added

--- opcode_funcitons.c
+++ opcode_funcitons.c
@@ -3,7 +3,15 @@
 /**
  * push -
  */
+void opcode_push(__attribute__((unused))stack_t **head_list, unsigned int line_number)
+{
+	printf("line: %d\n", line_number);
+}
 
 /**
  * pall
  */
+void opcode_pall(__attribute__((unused))stack_t **head_list, unsigned int line_number)
+{
+	printf("line: %d\n", line_number);
+}
