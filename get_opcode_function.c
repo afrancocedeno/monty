ef63707560f71b0d188efd1a89d4cb147037a996
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-20 20:33:47 +0000

    00 | file create & code try | funciton pointers try, error cant call a function

---
+++ get_opcode_function.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+
+void opcode_push(stack_t **head_list, unsigned int line_number);
+void opcode_pall(stack_t **head_list, unsigned int line_number);
+/**
+ * get_opcode_funciton -
+ * @head_list:
+ * @opcode_input:
+ * @line_number:
+ */
+void (*get_opcode_function(char *opcode_input))(stack_t **head_list, unsigned int line_number)
+{
+	instruction_t function[] = {
+	    {"push", opcode_push},
+	    {"pall", opcode_pall},
+	    {NULL, NULL}};
+	int i = 0;
+
+	/* find the input function in our opcode function */
+	for (; function[i].opcode != NULL && function[i].opcode != opcode_input; i++)
+		;
+	(function[i].f);
+	return EXIT_SUCCESS;
+}
+
+void opcode_push(__attribute__((unused))stack_t **head_list, unsigned int line_number)
+{
+	printf("line: %d\n", line_number);
+}
+
+void opcode_pall(__attribute__((unused))stack_t **head_list, unsigned int line_number)
+{
+	printf("line: %d\n", line_number);
+}
