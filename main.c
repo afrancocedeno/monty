a88bcea8596dfabdee0fa68162d1a587b42acb4b
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-20 20:32:04 +0000

    03 | code try | function pointer call using for loop

--- main.c
+++ main.c
@@ -7,26 +7,27 @@
  * 
  *
  */
-int main(__attribute__((unused)) int argc, char **argv)
+int main(__attribute__((unused))int argc, char **argv)
 {
 	FILE *file_pointer = NULL;
-	char *buffer = NULL, *function = NULL, *data = NULL;
+	char *buffer = NULL, *opcode = NULL;
 	size_t bytes_qty = 0;
+	stack_t *head_list = NULL;
+	int line_number = 1;
 
 	file_pointer = fopen(*(argv + 1), "r");
-	while (getline(&buffer, &bytes_qty, file_pointer) != EOF)
+	for (; getline(&buffer, &bytes_qty, file_pointer) != EOF; line_number++)
 	{
 		/* remove new line */
 		buffer = new_line_remove(buffer);
-		/* interprete monty opcode */
-		function = strtok(buffer, " ");
-		printf("%s\n", function);
-		data = strtok(NULL, " , \n");
-		printf("%s\n", data);
+		/* get opcode = pall or push */
+		opcode = strtok(buffer, " ");
+		/* get the opcode, no need to return */
+		get_opcode_function(opcode)(&head_list, line_number);
+		/* *data = NULL ??????????? data = strtok(NULL, " , \n");
+		printf("%s\n", data); */
 	}
 	fclose(file_pointer);
 	free(buffer);
 	return (0);
-	/* edge cases, no number argument */
-	/* edge case, more or less than two argvs */
 }
