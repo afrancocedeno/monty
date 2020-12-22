b325a3d75feef60e05dccf3b768e6996cf49504a
klich1984 <2239@holbertonschool.com>
2020-12-22 13:33:06 +0000

    02 | code update | implementation of push and pall functions

--- get_opcode_function.c
+++ get_opcode_function.c
@@ -1,23 +1,60 @@
 #include "monty.h"
 
-void opcode_push(stack_t **head_list, unsigned int line_number);
-void opcode_pall(stack_t **head_list, unsigned int line_number);
 /**
- * get_opcode_funciton -
- * @head_list:
- * @opcode_input:
- * @line_number:
- */
-void get_opcode_function(char *opcode_input, stack_t **head_list, unsigned int line_number)
+* get_opcode_function - missing description
+* @opcode_input: missing description
+* @head_list: missing description
+* @line_number: missing description
+* Return: Nothing
+*/
+void get_opcode_function(char *opcode_input, stack_t **head_list,
+	unsigned int line_number)
 {
 	instruction_t function[] = {
-	    {"push", opcode_push},
-	    {"pall", opcode_pall},
-	    {NULL, NULL}};
+		{"push", opcode_push},
+		{"pall", opcode_pall},
+		{NULL, NULL}
+	};
 	int i = 0;
+	char *str = NULL;
 
-	/* find the input function in our opcode function */
-	for (; function[i].opcode != NULL && *(function[i].opcode) != opcode_input; i++)
-		;
-	function[i].f(head_list, line_number);
+	while (function[i].opcode != NULL)
+	{
+		str = function[i].opcode;
+		/*if is = 0 the words are identical*/
+		if (strcmp(opcode_input, str) == 0)
+		{
+			function[i].f(head_list, line_number);
+			return;
+		}
+		i++;
+	}
+}
+
+/**
+* free_dlistint - function that free double link list
+* Return: Nothing
+*/
+
+void free_dlistint(void)
+{
+	stack_t *free_list = var_t.head_list;
+
+	if (free_list == NULL)
+		return;
+
+	if (free_list->prev != NULL)
+	{
+		while (free_list->prev != NULL)
+			free_list = free_list->prev;
+	}
+	else
+	{
+		while (free_list->next != NULL)
+		{
+			free_list = free_list->next;
+			free(free_list->prev);
+		}
+	}
+	free(free_list);
 }
