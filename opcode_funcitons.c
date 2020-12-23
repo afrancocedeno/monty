70b6587712169b89d56642607d7d31a6e578d99a
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-23 00:59:16 +0000

    15 | code try | .next on a different line

--- opcode_funcitons.c
+++ opcode_funcitons.c
@@ -92,7 +92,14 @@ void opcode_pop(stack_t **head_list, unsigned int line_number)
 		fprintf(stderr,	"L%d: can't pop an empty stack\n", line_number);
 		free(var_t.buffer);
 		fclose(var_t.file_pointer);
+		if (*head_list || head_list)
+			free_list(head_list);
 		exit(EXIT_FAILURE);
+	}
+	else if (top_element->next == NULL)
+	{
+		free(top_element);
+		free(head_list);
 	}
 	top_element->next->prev = NULL;
 	*head_list = (*top_element).next;
