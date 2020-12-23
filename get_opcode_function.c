134ccaf4e4747eaf2a6af108f90096afbd63bf69
klich1984 <2239@holbertonschool.com>
2020-12-23 04:30:24 +0000

    23 | update fix | update commit 15

--- get_opcode_function.c
+++ get_opcode_function.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 
 /**
-* get_opcode_function - missing descrip
+* get_opcode_function - missing descri
 * @opcode_input: missing description
 * @head_list: missing description
 * @line_number: missing description
